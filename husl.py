90392961899981daebbdcbafd8309e0bfff02b40
Chad Heyne <chadheyne@gmail.com>
2013-11-22 23:36:33 -0500

    Pep8 and Python3

--- husl.py
+++ husl.py
@@ -28,22 +28,45 @@ lab_k = 903.3
 
 # Public API
 
-def husl_to_rgb(h, s, l): return lch_to_rgb(*husl_to_lch([h, s, l]))
-def husl_to_hex(h, s, l): return rgb_to_hex(husl_to_rgb(h, s, l))
-
-def rgb_to_husl(r, g, b): return lch_to_husl(rgb_to_lch(r, g, b))
-def hex_to_husl(hex): return rgb_to_husl(*hex_to_rgb(hex))
-
-def huslp_to_rgb(h, s, l): return lch_to_rgb(*huslp_to_lch([h, s, l]))
-def huslp_to_hex(h, s, l): return rgb_to_hex(huslp_to_rgb(h, s, l))
-
-def rgb_to_huslp(r, g, b): return lch_to_huslp(rgb_to_lch(r, g, b))
-def hex_to_huslp(hex): return rgb_to_huslp(*hex_to_rgb(hex))
+def husl_to_rgb(h, s, l):
+    return lch_to_rgb(*husl_to_lch([h, s, l]))
 
 
+def husl_to_hex(h, s, l):
+    return rgb_to_hex(husl_to_rgb(h, s, l))
 
-def lch_to_rgb(l, c, h): return xyz_to_rgb(luv_to_xyz(lch_to_luv([l, c, h])))
-def rgb_to_lch(r, g, b): return luv_to_lch(xyz_to_luv(rgb_to_xyz([r, g, b])))
+
+def rgb_to_husl(r, g, b):
+    return lch_to_husl(rgb_to_lch(r, g, b))
+
+
+def hex_to_husl(hex):
+    return rgb_to_husl(*hex_to_rgb(hex))
+
+
+def huslp_to_rgb(h, s, l):
+    return lch_to_rgb(*huslp_to_lch([h, s, l]))
+
+
+def huslp_to_hex(h, s, l):
+    return rgb_to_hex(huslp_to_rgb(h, s, l))
+
+
+def rgb_to_huslp(r, g, b):
+    return lch_to_huslp(rgb_to_lch(r, g, b))
+
+
+def hex_to_huslp(hex):
+    return rgb_to_huslp(*hex_to_rgb(hex))
+
+
+def lch_to_rgb(l, c, h):
+    return xyz_to_rgb(luv_to_xyz(lch_to_luv([l, c, h])))
+
+
+def rgb_to_lch(r, g, b):
+    return luv_to_lch(xyz_to_luv(rgb_to_xyz([r, g, b])))
+
 
 def max_chroma(L, H):
     hrad = math.radians(H)
@@ -67,6 +90,7 @@ def max_chroma(L, H):
                 result = C
     return result
 
+
 def _hrad_extremum(L):
     lhs = (math.pow(L, 3.0) + 48.0 * math.pow(L, 2.0) + 768.0 * L + 4096.0) / 1560896.0
     rhs = 1107.0 / 125000.0
@@ -84,17 +108,20 @@ def _hrad_extremum(L):
             if limit == 0.0:
                 hrad += math.pi
             test = max_chroma(L, math.degrees(hrad))
-            if test < chroma: 
+            if test < chroma:
                 chroma = test
                 result = hrad
     return result
+
 
 def max_chroma_pastel(L):
     H = math.degrees(_hrad_extremum(L))
     return max_chroma(L, H)
 
+
 def dot_product(a, b):
     return sum(map(operator.mul, a, b))
+
 
 def f(t):
     if t > lab_e:
@@ -102,17 +129,20 @@ def f(t):
     else:
         return (7.787 * t + 16.0 / 116.0)
 
+
 def f_inv(t):
     if math.pow(t, 3.0) > lab_e:
         return (math.pow(t, 3.0))
     else:
         return (116.0 * t - 16.0) / lab_k
 
+
 def from_linear(c):
     if c <= 0.0031308:
         return 12.92 * c
     else:
         return (1.055 * math.pow(c, 1.0 / 2.4) - 0.055)
+
 
 def to_linear(c):
     a = 0.055
@@ -122,6 +152,7 @@ def to_linear(c):
     else:
         return (c / 12.92)
 
+
 def rgb_prepare(triple):
     ret = []
     for ch in triple:
@@ -129,54 +160,70 @@ def rgb_prepare(triple):
 
         if ch < -0.0001 or ch > 1.0001:
             raise Exception("Illegal RGB value %f" % ch)
-            
-        if ch < 0: ch = 0
-        if ch > 1: ch = 1
 
-        ret.append(round(ch * 255, 0))
+        if ch < 0:
+            ch = 0
+        if ch > 1:
+            ch = 1
+
+        # Fix for Python 3 which by default rounds 4.5 down to 4.0
+        # instead of Python 2 which is rounded to 5.0 which caused
+        # a couple off by one errors in the tests. Tests now all pass
+        # in Python 2 and Python 3
+        ret.append(round(ch * 255.001, 0))
 
     return ret
 
+
 def hex_to_rgb(hex):
-    if hex.startswith('#'): hex = hex[1:]
+    if hex.startswith('#'):
+        hex = hex[1:]
     r = int(hex[0:2], 16) / 255.0
     g = int(hex[2:4], 16) / 255.0
     b = int(hex[4:6], 16) / 255.0
     return [r, g, b]
 
+
 def rgb_to_hex(triple):
     [r, g, b] = triple
     return '#%02x%02x%02x' % tuple(rgb_prepare([r, g, b]))
 
+
 def xyz_to_rgb(triple):
     xyz = map(lambda row: dot_product(row, triple), m)
-    return map(from_linear, xyz)
+    return list(map(from_linear, xyz))
+
 
 def rgb_to_xyz(triple):
     rgbl = map(to_linear, triple)
-    return map(lambda row: dot_product(row, rgbl), m_inv)
+    return list(map(lambda row: dot_product(row, rgbl), m_inv))
+
 
 def xyz_to_luv(triple):
     X, Y, Z = triple
-    
-    if X == 0.0 and Y == 0.0 and Z == 0.0: return [0.0, 0.0, 0.0]
+
+    if X == 0.0 and Y == 0.0 and Z == 0.0:
+        return [0.0, 0.0, 0.0]
 
     varU = (4.0 * X) / (X + (15.0 * Y) + (3.0 * Z))
     varV = (9.0 * Y) / (X + (15.0 * Y) + (3.0 * Z))
     L = 116.0 * f(Y / refY) - 16.0
 
     # Black will create a divide-by-zero error
-    if L == 0.0: return [0.0, 0.0, 0.0]
+    if L == 0.0:
+        return [0.0, 0.0, 0.0]
 
     U = 13.0 * L * (varU - refU)
     V = 13.0 * L * (varV - refV)
 
     return [L, U, V]
 
+
 def luv_to_xyz(triple):
     L, U, V = triple
 
-    if L == 0: return [0.0, 0.0, 0.0]
+    if L == 0:
+        return [0.0, 0.0, 0.0]
 
     varY = f_inv((L + 16.0) / 116.0)
     varU = U / (13.0 * L) + refU
@@ -186,6 +233,7 @@ def luv_to_xyz(triple):
     Z = (9.0 * Y - (15.0 * varV * Y) - (varV * X)) / (3.0 * varV)
 
     return [X, Y, Z]
+
 
 def luv_to_lch(triple):
     L, U, V = triple
@@ -198,6 +246,7 @@ def luv_to_lch(triple):
 
     return [L, C, H]
 
+
 def lch_to_luv(triple):
     L, C, H = triple
 
@@ -207,45 +256,57 @@ def lch_to_luv(triple):
 
     return [L, U, V]
 
+
 def husl_to_lch(triple):
     H, S, L = triple
 
-    if L > 99.9999999: return [100, 0.0, H]
-    if L < 0.00000001: return [0.0, 0.0, H]
+    if L > 99.9999999:
+        return [100, 0.0, H]
+    if L < 0.00000001:
+        return [0.0, 0.0, H]
 
     mx = max_chroma(L, H)
     C = mx / 100.0 * S
 
     return [L, C, H]
 
+
 def lch_to_husl(triple):
     L, C, H = triple
 
-    if L > 99.9999999: return [H, 0.0, 100.0]
-    if L < 0.00000001: return [H, 0.0, 0.0]
-    
+    if L > 99.9999999:
+        return [H, 0.0, 100.0]
+    if L < 0.00000001:
+        return [H, 0.0, 0.0]
+
     mx = max_chroma(L, H)
     S = C / mx * 100.0
 
     return [H, S, L]
 
+
 def huslp_to_lch(triple):
     H, S, L = triple
 
-    if L > 99.9999999: return [100, 0.0, H]
-    if L < 0.00000001: return [0.0, 0.0, H]
+    if L > 99.9999999:
+        return [100, 0.0, H]
+    if L < 0.00000001:
+        return [0.0, 0.0, H]
 
     mx = max_chroma_pastel(L)
     C = mx / 100.0 * S
 
     return [L, C, H]
 
+
 def lch_to_huslp(triple):
     L, C, H = triple
 
-    if L > 99.9999999: return [H, 0.0, 100.0]
-    if L < 0.00000001: return [H, 0.0, 0.0]
-    
+    if L > 99.9999999:
+        return [H, 0.0, 100.0]
+    if L < 0.00000001:
+        return [H, 0.0, 0.0]
+
     mx = max_chroma_pastel(L)
     S = C / mx * 100.0
 
