e3479ec893cf525642bdd94f0a6dd32d897465a4
Sebastian Pipping <sebastian@pipping.org>
2022-06-05 22:17:19 +0200

    Drop support for end-of-life Python 3.6

--- setup.py
+++ setup.py
@@ -20,13 +20,12 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Software Development",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     setup_requires=[
         'setuptools>=38.6.0',  # for long_description_content_type
     ],
